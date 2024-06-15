211f7e27d74f349dddde033d82a178c6abdae357
MPenava <marko.penava@fpmoz.sum.ba>
2024-06-15 15:02:05 +0200

    build: setup url base for development and production

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,12 @@ import react from "@astrojs/react";
 
 import tailwind from "@astrojs/tailwind";
 
+// Check if the environment is development or production
+const isDevelopment = process.env.NODE_ENV === "development";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://mpenava.github.io",
-  base: "project_setup_react-astro-tailwind",
+  base: isDevelopment ? "" : "project_setup_react-astro-tailwind",
   integrations: [react(), tailwind()],
 });
